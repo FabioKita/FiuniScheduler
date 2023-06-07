f843a4c62a2b08d1eb0da5d839ba2467ac221782
FabioKita <fabio.kita@fiuni.edu.py>
2023-06-07 12:47:17 -0400

    added check component

---
+++ src/components/card/common/card-check.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Animated from "react-native-reanimated";
+
+const CardCheck = ({
+    active,
+    setActive = ()=>{}
+})=>{
+    return <Animated.View>
+
+    </Animated.View>
+}
+
+export default CardCheck;
