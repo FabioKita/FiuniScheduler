6203f18175194e8d1b3b0dcb9cb933014a4b227c
FabioKita <fabio.kita@fiuni.edu.py>
2023-06-06 22:48:23 -0400

    some testing stuffs

--- src/screens/entries/reminders.jsx
+++ src/screens/entries/reminders.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
+import TestCard from "src/components/card/test-card";
 import ColorContainer from "src/components/color-container";
 
 const Reminders = ({
     navigation
 }) => {
     return <ColorContainer style={styles.Container} color="#AED581">
-        <Text>
-            Reminders
-        </Text>
+        <View style={styles.List}>
+            <TestCard/>
+            <TestCard/>
+            <TestCard/>
+        </View>
     </ColorContainer>
 }
 
@@ -16,8 +19,14 @@ const styles = StyleSheet.create({
     Container: {
         flex: 1,
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
+        padding:24
+    },
+    List:{
+        display: "flex",
+        flexDirection:"column",
+        alignItems:"stretch",
+        justifyContent:"flex-start",
+        gap:24
     }
 })
 
